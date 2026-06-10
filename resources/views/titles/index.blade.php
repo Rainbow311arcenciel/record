48449520d2188a3f99f82fdaf1ec880a159bc48d
Rainbow5883 <s.y.n.junko.311@docomo.ne.jp>
2026-06-10 20:24:31 +0900

    記録を追加の項目を削除

--- resources/views/titles/index.blade.php
+++ resources/views/titles/index.blade.php
@@ -14,9 +14,6 @@
                 <a href="{{ route('records.index', $title->id) }}">
                     {{ $title->name }}
                 </a>
-                <a href="{{ route('records.create', $title) }}">
-                 記録を追加
-                </a>
             </td>
         </tr>
     @endforeach
