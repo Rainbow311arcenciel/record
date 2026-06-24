e1c9b333dee2f63dc17e0bc788d3953efc2770e9
中村祐馬 <yuma.nakamura.0309@gmail.com>
2026-06-24 20:29:28 +0900

    mainブランチをPR #2マージ後の正しい状態に修正

--- app/Http/Controllers/RecordController.php
+++ app/Http/Controllers/RecordController.php
@@ -16,21 +16,3 @@ class RecordController extends Controller
         ));
     }
 }
-public function create($title)
-{
-    return view('records.create', compact('title'));
-}
-
-public function store(Request $request, $title)
-{
-    $validated = $request->validate([
-        'date' => ['required', 'date'],
-        'amount' => ['required', 'integer'],
-        'comment' => ['nullable', 'string', 'max:255'],
-    ]);
-
-    // 保存処理
-    // Record::create([...]);
-
-    return redirect()->route('records.index', $title);
-}
--- routes/web.php
+++ routes/web.php
@@ -10,19 +10,3 @@ Route::get(
     '/titles/{title}/records',
     [RecordController::class, 'index']
 )->name('records.index');
-use App\Http\Controllers\RecordController;
-
-Route::get(
-    '/titles/{title}/records',
-    [RecordController::class, 'index']
-)->name('records.index');
-
-Route::get(
-    '/titles/{title}/records/create',
-    [RecordController::class, 'create']
-)->name('records.create');
-
-Route::post(
-    '/titles/{title}/records',
-    [RecordController::class, 'store']
-)->name('records.store');
