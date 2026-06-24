a6cbd0bb3dc714fa347efe1b56b42e7b695ba436
中村祐馬 <yuma.nakamura.0309@gmail.com>
2026-06-24 20:43:50 +0900

    Add record creation form

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,5 @@ use App\Http\Controllers\RecordController;
 
 Route::get('/titles', [TitleController::class, 'index']);
 
-Route::get(
-    '/titles/{title}/records',
-    [RecordController::class, 'index']
-)->name('records.index');
+Route::post('/records', [RecordController::class, 'store'])
+    ->name('records.store');
